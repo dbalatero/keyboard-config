f699fac2e1bdc1af47dbefde28c1b2d448b646a4
David Balatero <dbalatero@stripe.com>
2020-08-24 13:28:35 -0400

    basic layer, enable flashing the dactyl

--- dactyl_6x6/keymap.c
+++ dactyl_6x6/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
@@ -8,19 +7,26 @@
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+// TODO
+// super
+// hyper
+// fn key
+// arrow key layer
+// leader key for layer?
 
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_6x6(
 
-     KC_F1  , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                         KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,KC_F12 ,
-     KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-     KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                      KC_LBRC,KC_RBRC,                                                       KC_PLUS, KC_EQL,
-                                      RAISE,KC_SPC,                        KC_ENT, LOWER,
-                                      KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
-                                      KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
+     KC_ESC    , KC_F2 , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,                         KC_F7 , KC_F8 , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
+     KC_EQUAL  , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8   , KC_9   , KC_0   , KC_MINUS,
+     KC_TAB    , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I   , KC_O   , KC_P   , KC_BSLASH,
+     KC_LCTRL  , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+     KC_LSHIFT , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  , KC_COMM, KC_DOT , KC_SLSH, KC_RSHIFT,
+                        _______, KC_GRAVE,                                                       KC_LBRC, KC_RBRC ,
+
+                                      KC_LCTRL, KC_LGUI,                        KC_BSPACE, KC_SPACE,
+                                        _______, KC_LALT,                     _______,  _______,
+                                          KC_BSPC, KC_GRV,                  _______, KC_F1
   ),
 
   [_LOWER] = LAYOUT_6x6(
@@ -49,5 +55,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                _______,_______,            _______,_______,
                                                _______,_______,            _______,_______
   ),
-
 };
