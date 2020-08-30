ad42ac1715369dff17793f36a9a97e23fa78f31e
David Balatero <dbalatero@gmail.com>
2020-08-29 23:35:43 -0400

    Fix passthru

--- dactyl_6x6/keymap.c
+++ dactyl_6x6/keymap.c
@@ -84,6 +84,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         disable_raise_layer();
       }
+
+      return false;
   }
 
   // Pass through all other keys
