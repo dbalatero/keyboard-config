9860b04941e6ce8e31d606c2227c5753dc267b9f
David Balatero <db@graphite.dev>
2023-11-29 10:35:40 -0500

    new one

--- imprint_function_row_full_bottom_row/rules.mk
+++ imprint_function_row_full_bottom_row/rules.mk
@@ -1,9 +1,1 @@
-VIA_ENABLE = yes
-VIAL_ENABLE = yes
-LTO_ENABLE = no
-QMK_SETTINGS = yes
-TAP_DANCE_ENABLE = yes
-COMBO_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
-VIALRGB_ENABLE = yes
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
