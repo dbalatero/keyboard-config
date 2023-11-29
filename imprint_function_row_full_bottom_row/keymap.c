7321709e044568a5018e5c4f355095f02b4899b9
David Balatero <db@graphite.dev>
2023-11-29 10:45:17 -0500

    wiring

--- imprint_function_row_full_bottom_row/keymap.c
+++ imprint_function_row_full_bottom_row/keymap.c
@@ -103,10 +103,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,                                                                 KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,       KC_BSLS,
         KC_ENTER, KC_A,     KC_S,     KC_D,    KC_F,     KC_G,                                                                 KC_H,    KC_J,  KC_K,    KC_L,    HYPER_SEMI, KC_QUOT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,                                                                 KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,
-        //                                                     1       2        3                     7            8        9
-        _______,  _______,  KC_GRAVE, KC_LEFT, KC_RIGHT,    KC_LCTL, KC_LGUI, _______,        TOGGLE_RAISE, KC_BSPC, KC_SPC,       KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,    _______,
+        //                                                     1       2        3                     7        8        9
+        _______,  _______,  KC_GRAVE, KC_LEFT, KC_RIGHT,    KC_LCTL, KC_LGUI, _______,        TOGGLE_RAISE, KC_BSPC, KC_SPC,            KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,    _______,
         //                                                  4         5        6                   10       11      12
-                                                        KC_LALT, TOGGLE_RAISE, KC_HOME,               KC_END,  KC_F12,  MOD_SUPER
+                                                        KC_LALT, TOGGLE_RAISE, KC_HOME,           KC_END,  KC_F12,  MOD_SUPER
     ),
 
     [_RAISE] = LAYOUT_fun_full_bottom_row(
