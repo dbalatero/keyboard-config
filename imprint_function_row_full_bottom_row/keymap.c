eea22a7633a1fea6527a5cef882f16114be1a1a2
David Balatero <db@graphite.dev>
2023-11-29 13:50:06 -0500

    current

--- imprint_function_row_full_bottom_row/keymap.c
+++ imprint_function_row_full_bottom_row/keymap.c
@@ -80,17 +80,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //
 //         +----+
 // +----+  |    |                                          +-----+
-// |    |  | 3  | +----+                                   |     |
-// | 2  |  |    | |    |                           +-----+ |  8  |
-// |    |  +----+ | 6  |                           |     | |     |
-// +----+         |    |                    +----+ | 7   | +-----+
+// |    |  | 2  | +----+                                   |     |
+// | 1  |  |    | |    |                           +-----+ |  9  |
+// |    |  +----+ | 3  |                           |     | |     |
+// +----+         |    |                    +----+ | 8   | +-----+
 //    +----+      +----+                    |    | |     |
-//    | 1  |  +----+                        |10  | +-----+        +----+
-//    |    |  | 4  |                        |    |        +----+  |    |
-//    +----+  |    |                        +----+        |    |  | 9  |
-//            +----++-----+                        +----+ |12  |  +----+
+//    | 4  |  +----+                        | 7  | +-----+        +----+
+//    |    |  | 5  |                        |    |        +----+  |    |
+//    +----+  |    |                        +----+        |    |  |12  |
+//            +----++-----+                        +----+ |11  |  +----+
 //                  |     |                        |    | +----+
-//                  | 5   |                        |11  |
+//                  | 6   |                        |10  |
 //                  +-----+                        +----+
 //
 
@@ -104,9 +104,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ENTER, KC_A,     KC_S,     KC_D,    KC_F,     KC_G,                                                                 KC_H,    KC_J,  KC_K,    KC_L,    HYPER_SEMI, KC_QUOT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,                                                                 KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,
         //                                                     1       2        3                     7            8        9
-        _______,  _______,  KC_GRAVE, KC_LEFT, KC_RIGHT,    KC_LCTL, KC_LALT, TOGGLE_RAISE,        TOGGLE_RAISE, MOD_SUPER, KC_SPC,     KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,    _______,
+        _______,  _______,  KC_GRAVE, KC_LEFT, KC_RIGHT,    KC_LALT, TOGGLE_RAISE, KC_HOME,        KC_END, TOGGLE_RAISE, MOD_SUPER,     KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,    _______,
         //                                                  4        5        6                   10       11      12
-                                                        KC_LGUI, _______, KC_HOME,           KC_END,  KC_F12,  KC_BSPC
+                                                        KC_LCTL, KC_LGUI, _______,           KC_F12,  KC_BSPC,  KC_SPC
     ),
 
     [_RAISE] = LAYOUT_fun_full_bottom_row(
