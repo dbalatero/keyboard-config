3b55c9584481590d028b23ecb76f31feb8d85735
David Balatero <db@graphite.dev>
2024-01-28 13:45:31 -0500

    fix

--- imprint_function_row_full_bottom_row/keymap.c
+++ imprint_function_row_full_bottom_row/keymap.c
@@ -104,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ENTER, KC_A,     KC_S,     KC_D,    KC_F,     KC_G,                                                                 KC_H,    KC_J,    KC_K,    KC_L,    HYPER_SEMI, KC_QUOT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,                                                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,
         //                                                     1       2        3                     7            8        9
-        _______,  _______,  KC_GRAVE, KC_LEFT, KC_RIGHT,    KC_LALT, TOGGLE_RAISE, KC_HOME,        KC_END, TOGGLE_RAISE, MOD_SUPER,     KC_DOWN, KC_UP,   KC_LBRC, KC_RBRC,    _______,
+        _______,  _______,  KC_GRAVE, KC_LEFT, KC_RIGHT,    KC_LALT, TOGGLE_RAISE, KC_HOME,        KC_END, TOGGLE_RAISE, MOD_SUPER,     KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,    _______,
         //                                                  4        5        6                   10       11      12
                                                         KC_LCTL, KC_LGUI, _______,           KC_F12,  KC_BSPC,  KC_SPC
     ),
