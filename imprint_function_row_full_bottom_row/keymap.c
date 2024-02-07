fb320d7d12ac2836a6297134b51a89bf92444bdd
David Balatero <db@graphite.dev>
2024-02-06 23:33:08 -0500

    remove layer toggle, don't need it anymore yay

--- imprint_function_row_full_bottom_row/keymap.c
+++ imprint_function_row_full_bottom_row/keymap.c
@@ -13,6 +13,7 @@ enum custom_keycodes {
 #define _RAISE 1
 
 #define HYPER_SEMI MT(MOD_HYPR, KC_SEMICOLON)
+#define FN_KEY MO(_RAISE)
 
 void enable_raise_layer(void);
 void disable_raise_layer(void);
@@ -103,19 +104,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,                                                                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSLS,
         KC_ENTER, KC_A,     KC_S,     KC_D,    KC_F,     KC_G,                                                                 KC_H,    KC_J,    KC_K,    KC_L,    HYPER_SEMI, KC_QUOT,
         KC_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,                                                                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,
-        //                                                     1       2        3                     7            8        9
-        _______,  _______,  KC_GRAVE, KC_LEFT, KC_RIGHT,    KC_LALT, TOGGLE_RAISE, KC_HOME,        KC_END, TOGGLE_RAISE, MOD_SUPER,     KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,    _______,
-        //                                                  4        5        6                   10       11      12
+        //                                                     1       2        3          7       8        9
+        _______,  _______,  KC_GRAVE, KC_LEFT, KC_RIGHT,    KC_LALT, FN_KEY,   KC_HOME,    KC_END, FN_KEY, MOD_SUPER,                   KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC,    _______,
+        //                                                  4        5        6                10       11      12
                                                         KC_LCTL, KC_LGUI, _______,           KC_F12,  KC_BSPC,  KC_SPC
     ),
 
     [_RAISE] = LAYOUT_fun_full_bottom_row(
-        _______, _______, _______, _______, _______, _______,                                                                  _______, _______, _______,      _______,  _______,  _______,
+        _______, KC_BRMD, KC_BRMU, _______, _______, _______,                                                                  _______, KC_MPRV, KC_MPLY,      KC_MNXT,  KC_VOLD,  KC_MUTE,
         KC_VOLU, _______, _______, _______, _______, _______,                                                                  _______, _______, _______,      _______,  _______,  KC_VOLD,
-        _______, _______, _______, _______, _______, _______,                                                                  _______, _______, TOGGLE_RAISE, KC_END,   _______,  _______,
+        _______, _______, _______, _______, _______, _______,                                                                  _______, _______, _______,      KC_END,   _______,  _______,
         _______, _______, _______, _______, _______, _______,                                                                  KC_LEFT, KC_DOWN, KC_UP,        KC_RIGHT, _______,  _______,
         _______, _______, _______, _______, _______, _______,                                                                  _______, KC_MUTE, _______,      _______,  _______,  _______,
         _______, _______, _______, _______, _______,      _______, _______, _______,         _______, _______, _______,                 _______, _______,      _______,  _______,  _______,
-                                                      _______, _______, _______,                _______, _______, _______
+                                                      _______, _______, _______,                KC_VOLU, _______, _______
     )
 };
